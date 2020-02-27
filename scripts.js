f948a5f9e5c0f2600a20664a523bda324a4b914d
Galaxy-Prism <32467979+Galaxy-Prism@users.noreply.github.com>
2020-02-26 19:57:53 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,15 @@
 function makeFood() {
   console.log("Food");
 }
+
+var hideyToggle = 0;
+function hideyText() {
+  if(hideyToggle == 0) {
+    document.getElementById("hideyID").innerHTML = "This text is hiding now";
+    hideyToggle = 1;
+  }
+  if(hideyToggle == 1) {
+    document.getElementById("hideyID").innerHTML = "I am not really sure how to get the text normal again. Sorry for the inconvenience?";
+    hideyToggle = 0;
+  }
+}
