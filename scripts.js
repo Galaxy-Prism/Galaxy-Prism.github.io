1eaa31a5f6469537b0c0309583ebb3f97e809af8
Galaxy-Prism <32467979+Galaxy-Prism@users.noreply.github.com>
2021-01-29 09:28:11 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,1 @@
-var hideyInt = 0;
 
-function makeFood() {
-  console.log("Food");
-}
-
-function hideyText() {
-  if(hideyText != 1) {
-    window.alert("Oh. You broke my site. It should repair if you refresh.");
-    document.getElementById("hideyID").innerHTML = "I am not really sure how to get the text normal again. Sorry for the inconvenience?";
-    hideyInt = 1;
-  }
-}
